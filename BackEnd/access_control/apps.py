0e16b179abf84f1205bd4be39a449665ed95503e
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2024-04-16 10:42:26 +0700

    Remove Old BackEnd code

--- BackEnd/access_control/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccessControlConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'access_control'
--- BackEnd/appliances/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Appliance
-
-class ApplianceAdmin(admin.ModelAdmin):
-    list_display = ('appliance_id', 'name', 'status')
-    list_filter = ('status',)
-    search_fields = ('appliance_id', 'name')
-
-admin.site.register(Appliance, ApplianceAdmin)
