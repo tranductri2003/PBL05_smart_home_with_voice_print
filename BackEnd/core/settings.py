2b552558d7e59adc1ebfcdd5b8c7d4b73472dd91
Phatdtvdvt <phatanhpham2003@gmail.com>
2024-04-25 18:45:02 +0700

    update web tranfer audio to vector

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -123,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
