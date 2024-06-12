441a22961c423569429d6c2ba79557024552d170
tranductri2003 <tranductri2003@gmail.com>
2024-06-12 12:27:14 +0700

    Update code

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Tải các biến môi trường từ tệp .env
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1bzmxthl4iv_+nmb20(ofq7)(gbl0*e-=oq+givgb!-8f@$6^y'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
